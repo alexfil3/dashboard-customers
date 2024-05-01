439fd6c37ca07f0bd9d7dd9629f094223ab79d7b
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-05-01 23:23:13 +0300

    delete extra files

--- draft/main.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const pug = require("pug");
-const data = require("./data");
-const user = require("./user/user.js");
-
-let html = pug.renderFile("index.pug", { data, user });
-fs.writeFileSync("index.html", html);
--- draft/user/user.js
+++
@@ -1,7 +0,0 @@
-const user = {
-  name: "Evano",
-  profession: "Project Manager",
-  img: "./user.jpg",
-};
-
-module.exports = user;
