b297bfa6d89a0e108695d797c28f00aef3072780
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-05-14 08:19:51 +0300

    fix body action on open mob menu

--- mob-blocks/menu/menu.js
+++ mob-blocks/menu/menu.js
@@ -1,13 +1,16 @@
 const buttonOpen = document.querySelector("#open");
 const buttonClose = document.querySelector("#close");
 const menu = document.querySelector(".menu");
+const body = document.querySelector("body");
 
 buttonOpen.addEventListener("click", function () {
   menu.classList.add("show");
   buttonClose.classList.add("show-button");
+  body.classList.add("scroll-off");
 });
 
 buttonClose.addEventListener("click", function () {
   menu.classList.remove("show");
   buttonClose.classList.remove("show-button");
+  body.classList.remove("scroll-off");
 });
